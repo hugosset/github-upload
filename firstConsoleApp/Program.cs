1947c98f680b69c9864e1c4269252e01560b3bb6
hugosset <hugossetiawan@gmail.com>
2020-03-20 13:14:00 +0700

    init repo

---
+++ firstConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello;
+            hello = "Hello world!";
+            Console.WriteLine(hello);
+            Console.WriteLine("This is the third line of code on C# .NET framework!!!");
+        }
+    }
+}
